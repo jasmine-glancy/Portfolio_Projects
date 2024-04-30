f1b9a2d9ee4ba3f69600716c4544ae55e03102f6
jglancy95 <jasmine.glancy.vt@gmail.com>
2024-04-30 19:36:38 -0400

    Rearrange project display order, customize colors based on images used

--- JGL Portfolio Website/main.py
+++ JGL Portfolio Website/main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 import requests
@@ -11,4 +12,11 @@ Bootstrap5(app)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    """Shows portfolio homepage with a current copyright date"""
+    
+    # Convert current date to a string
+    jgl_date_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+    
+    # Grab only the year
+    jgl_current_year = jgl_date_str[:4]
+    return render_template("index.html", date=jgl_current_year)
