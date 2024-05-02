1f5e53d1135a2f815bdcf6904eb80c42956bf1f0
jglancy95 <jasmine.glancy.vt@gmail.com>
2024-05-01 23:10:28 -0400

    Add TODOs/pseudocode

---
+++ JGL Tic Tac Toe/main.py
@@ -0,0 +1,16 @@
+"""A text-based tic tac toe game!"""
+
+# Two players, x and o
+
+# There's a board with 9 boxes +/- nested array?
+
+# TODO: Ask user to choose a symbol
+
+# TODO: Ask user to place their sign in the available empty boxes
+
+# TODO: Second player chooses a sign from an available empty box
+
+# Goal is to place respective signs completely row-wise, column-wise, or diagonally
+
+# TODO: Game continues until it ends in a draw or a win
+    # It's a draw if all the boxes are filled without either player getting a winning match
