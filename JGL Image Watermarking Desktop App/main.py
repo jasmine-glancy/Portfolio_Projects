a4ff075721960e9266e5ea94b21e356c06dd2699
jglancy95 <jasmine.glancy.vt@gmail.com>
2024-05-16 00:29:53 -0400

    Add TODOs

---
+++ JGL Image Watermarking Desktop App/main.py
@@ -0,0 +1,27 @@
+"""A GUI that allows you to watermark an image!"""
+
+# To-do's adapted from suggestions at https://www.geeksforgeeks.org/python-pillow-creating-a-watermark/
+# TODO: 1. Import libraries for image processing
+
+# TODO: Import libraries for GUI (TKinter)
+
+# TODO: 2. Use image.open() for the opening of the mage and image.show() for the photo view to open
+
+# TODO: 3. plt.imshow() is used to open the image in the IDE
+
+# TODO: 4. Make a copy of an image for the creation of the watermark image
+
+# TODO: 5. Make the image editable using ImageDraw
+
+# TODO: 6. Use ImageFont to specify font and font size
+
+# TODO: 7. Create a draw method of ImageDraw module and pass the image as a parameter in the function
+
+# TODO: 8. Create a Font using ImageFont module function truetypel() as it neads two parameters
+    # i.e. ("font type", size)
+    
+# TODO: 9. Then use text() function of draw object and pass the 4 parameters
+    # the 4 parameters should be (point of starting for text, "sample text", Color, ImageFont object)
+    
+# TODO: 10. plt.Imshow(watermark_image) for the output
+
