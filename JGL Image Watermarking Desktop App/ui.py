18b8f0bad72bc913334ebfd3492b07212de1c2ed
jglancy95 <jasmine.glancy.vt@gmail.com>
2024-06-20 18:35:32 -0400

    Add file choosing system, watermarking still WIP

--- JGL Image Watermarking Desktop App/ui.py
+++ JGL Image Watermarking Desktop App/ui.py
@@ -2,12 +2,15 @@
 
 
 # Import libraries for GUI (TKinter)
+import matplotlib.pyplot as plt
 import tkinter as tk
-from PIL import Image
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+from PIL import Image, ImageDraw, ImageFont
 
-    
+JGL_IMAGE_FOLDER = "../pictures/coding"
+
 class JglUserInterface():
-    def __init__(self):
+    def __init__(self) -> None:
         self.jgl_window = tk.Tk()
         self.jgl_window.title("Image Watermarker")
         self.jgl_window.minsize(width=755, height=375)
@@ -16,32 +19,40 @@ class JglUserInterface():
         self.jgl_ask_user_for_image()
         self.jgl_ask_for_watermark()
         self.jgl_add_watermark()
+        self.jgl_verify_watermark_info()
         self.jgl_window.mainloop()
         
-    def jgl_load_logo(self):
+    def jgl_load_logo(self) -> None:
         """Adds the app logo"""
         jgl_canvas = tk.Canvas(width=950, height=525, bg="#FFF5D6", highlightthickness=0)
         self.jgl_logo = tk.PhotoImage(file="D:/Work/Portfolio Projects/JGL Image Watermarking Desktop App/icon.png")
         jgl_canvas.create_image(475, 225, image=self.jgl_logo)
         jgl_canvas.grid(column=0, row=0, columnspan=4)
         
-    def jgl_ask_user_for_image(self):
+    def jgl_ask_user_for_image(self) -> None:
         """Asks user for the path to the image"""
         
         # Image path label
         jgl_image_label = tk.Label()
-        jgl_image_label.config(text="Image name (i.e. file.png): ", 
+        jgl_image_label.config(text="Please choose an image: ", 
                                bg="#FFF5D6", 
                                fg="#968C6D",
                                font=("Helvetica", 12, "bold"))
         jgl_image_label.grid(column=0, row=1)
         
-        # Image path input
-        self.jgl_image_path_entry = tk.Entry(width=35, bg="#FFF8EB")
-        self.jgl_image_path_entry.grid(column=1, row=1, columnspan=2)
+        # Browsing button and notifier
+        self.jgl_browsing = tk.StringVar()
+        jgl_find_file_button = tk.Button(self.jgl_window,
+                                         command=self.jgl_open_file,
+                                         textvariable=self.jgl_browsing,
+                                         font="Helvetica",
+                                         bg="#968C6D",
+                                         fg="#FFF5D6",
+                                         width=53)
+        self.jgl_browsing.set("Open File Explorer")
+        jgl_find_file_button.grid(column=1, row=1, columnspan=3)
         
-        
-    def jgl_ask_for_watermark(self):
+    def jgl_ask_for_watermark(self) -> None:
         """Ask for user's watermark preferences"""
         
         # Watermark preferences label
@@ -62,33 +73,8 @@ class JglUserInterface():
         jgl_watermark_text_label.grid(column=0, row=3)
         
         # Font watermark input
-        jgl_watermark_text = tk.Entry(width=98, bg="#FFF8EB")
-        jgl_watermark_text.grid(column=1, row=3, columnspan=3)
-        
-        # Font X-coordinate label
-        jgl_font_xcord_label = tk.Label()
-        jgl_font_xcord_label.config(text="Please enter the x-coordinate of the text: ", 
-                               bg="#FFF5D6", 
-                               fg="#968C6D",
-                               font=("Helvetica", 12, "bold"))
-        jgl_font_xcord_label.grid(column=0, row=4)
-        
-        # Font X-coordinate input
-        jgl_font_xcord_entry = tk.Entry(width=10, bg="#FFF8EB")
-        jgl_font_xcord_entry.grid(column=1, row=4)
-        
-        # Font Y-coordinate label
-        jgl_font_ycord_label = tk.Label()
-        jgl_font_ycord_label.config(text="Please enter the y-coordinate of the text: ", 
-                               bg="#FFF5D6", 
-                               fg="#968C6D",
-                               font=("Helvetica", 12, "bold"),
-                               padx=25)
-        jgl_font_ycord_label.grid(column=2, row=4)
-        
-        # Font Y-coordinate input
-        jgl_font_ycord_entry = tk.Entry(width=10, bg="#FFF8EB")
-        jgl_font_ycord_entry.grid(column=3, row=4, padx=25)
+        self.jgl_watermark_text = tk.Entry(width=98, bg="#FFF8EB")
+        self.jgl_watermark_text.grid(column=1, row=3, columnspan=3)
         
         # Font fill red value label
         jgl_font_fill_r_label = tk.Label()
@@ -99,8 +85,8 @@ class JglUserInterface():
         jgl_font_fill_r_label.grid(column=0, row=5)
         
         # Font fill red value input
-        jgl_font_fill_r_entry = tk.Entry(width=10, bg="#FFF8EB")
-        jgl_font_fill_r_entry.grid(column=1, row=5)
+        self.jgl_font_fill_r_entry = tk.Entry(width=10, bg="#FFF8EB")
+        self.jgl_font_fill_r_entry.grid(column=1, row=5)
         
         # Font fill green value label
         jgl_font_fill_g_label = tk.Label()
@@ -112,8 +98,8 @@ class JglUserInterface():
         jgl_font_fill_g_label.grid(column=2, row=5)
         
         # Font fill green value input
-        jgl_font_fill_g_entry = tk.Entry(width=10, bg="#FFF8EB")
-        jgl_font_fill_g_entry.grid(column=3, row=5, padx=25)
+        self.jgl_font_fill_g_entry = tk.Entry(width=10, bg="#FFF8EB")
+        self.jgl_font_fill_g_entry.grid(column=3, row=5, padx=25)
         
         # Font fill blue value label
         jgl_font_fill_b_label = tk.Label()
@@ -125,90 +111,127 @@ class JglUserInterface():
         jgl_font_fill_b_label.grid(column=0, row=6)
         
         # Font fill blue value input
-        jgl_font_fill_b_entry = tk.Entry(width=10, bg="#FFF8EB")
-        jgl_font_fill_b_entry.grid(column=1, row=6)
+        self.jgl_font_fill_b_entry = tk.Entry(width=10, bg="#FFF8EB")
+        self.jgl_font_fill_b_entry.grid(column=1, row=6)
         
-        # Font style label
-        jgl_style_label = tk.Label()
-        jgl_style_label.config(text="Please enter font size (i.e. 10): ", 
+        # Font size label
+        jgl_size_label = tk.Label()
+        jgl_size_label.config(text="Please enter font size (i.e. 10): ", 
                                bg="#FFF5D6", 
                                fg="#968C6D",
                                font=("Helvetica", 11, "bold"))
-        jgl_style_label.grid(column=2, row=6)
+        jgl_size_label.grid(column=2, row=6)
         
-        # Font style input
-        jgl_style_entry = tk.Entry(width=10, bg="#FFF8EB")
-        jgl_style_entry.grid(column=3, row=6, padx=25)
+        # Font size input
+        self.jgl_size_entry = tk.Entry(width=10, bg="#FFF8EB")
+        self.jgl_size_entry.grid(column=3, row=6, padx=25)
+    
         
-        # Font anchor label
-        jgl_anchor_label = tk.Label()
-        jgl_anchor_label.config(text="Please enter font anchor (i.e. lt for vertical, la for horizontal): ", 
-                               bg="#FFF5D6", 
-                               fg="#968C6D",
-                               font=("Helvetica", 11, "bold"))
-        jgl_anchor_label.grid(column=0, row=7)
-        
-        # Font anchor input
-        jgl_anchor_entry = tk.Entry(width=10, bg="#FFF8EB")
-        jgl_anchor_entry.grid(column=1, row=7, padx=25)
-        
-        # Font alignment label
-        jgl_anchor_label = tk.Label()
-        jgl_anchor_label.config(text="Please enter font alignment (i.e. center, left, right): ", 
-                               bg="#FFF5D6", 
-                               fg="#968C6D",
-                               font=("Helvetica", 11, "bold"))
-        jgl_anchor_label.grid(column=2, row=7)
-        
-        # Font alignment input
-        jgl_anchor_entry = tk.Entry(width=10, bg="#FFF8EB")
-        jgl_anchor_entry.grid(column=3, row=7, padx=25)
-        
-    def jgl_add_watermark(self):
+    def jgl_add_watermark(self) -> None:
         """Takes info and marks it on the image the user inputs"""
         
         # Watermark Button
         jg_watermark_button = tk.Button(text="Watermark", width=35, highlightthickness=0, 
                                      bg="#968C6D", fg="#FFF8EB", pady=10, padx=10,
-                                     command=self.jgl_watermark)
+                                     command=self.jgl_verify_watermark_info)
         jg_watermark_button.grid(column=0, row=8, columnspan=4, pady=25)
 
-    def jgl_watermark(self):
+    def jgl_verify_watermark_info(self) -> None:
         """Watermarks the image"""
-        jgl_image_to_mark = self.jgl_image_path_entry.get()
-        print("Tapped!")
-        print(jgl_image_to_mark)   
         
-        #------------  Watermark Image & Saving it ----------------
-
+        # Gets the data input from the form
+        jgl_watermark_text = self.jgl_watermark_text.get()
+        
+        
         try:
-            # 2. Use image.open() for the opening of the image 
-            with Image.open(jgl_image_to_mark) as jgl_img:
-                # Opens the photo viewer
-                jgl_img.show()
-        except FileNotFoundError as jgl_not_found:
-            print(f"File not found, exception: {jgl_not_found}")
+            jgl_font_size = int(self.jgl_size_entry.get())
         except Exception as e:
-            print(f"Can't open file, exception: {e}")
-
-        # TODO: 3. plt.imshow() is used to open the image in the IDE
-
-        # TODO: 4. Make a copy of an image for the creation of the watermark image with the same dimensions as the older one
-
-        # TODO: 5. Make the image editable using ImageDraw
-
-        # TODO: 6. Use ImageFont to specify font and font size
-
-        # TODO: 7. Create a draw method of ImageDraw module and pass the image as a parameter in the function
-
-        # TODO: 8. Create a Font using ImageFont module function truetypel() as it neads two parameters
-            # i.e. ("font type", size)
+            print(f"Please enter a size greater than 0. Exception: {e}")
             
-        # TODO: 9. Then use text() function of draw object and pass the 4 parameters
-            # the 4 parameters should be (point of starting for text, "sample text", Color, ImageFont object)
+        try:
+            # Use ImageFont to specify font and font size
+            jgl_font = ImageFont.truetype("arial.ttf", jgl_font_size)
             
-        # TODO: 10. plt.Imshow(watermark_image) for the output
-    
+        except Exception as e:
+            print(f"Can't choose font, exception: {e}")
+            
+        try:
+            self.jgl_color_choice = (int(self.jgl_font_fill_r_entry.get()), int(self.jgl_font_fill_g_entry.get()), int(self.jgl_font_fill_b_entry.get()))
+        except Exception as e:
+            print(f"Can't choose color, exception: {e}")
+            
+        try:
+            transparent = Image.new('RGBA', self.jgl_original_image.size, (0,0,0,0))
+            transparent.paste(self.jgl_original_image, (0, 0))
+            transparent.paste(self.jgl_img_copy_mask, self.position, mask=self.jgl_img_copy_mask)
+            transparent.show()
+            
+            # Use text() function of draw object
+
+            self.jgl_drawing.text(self.position,
+                            jgl_watermark_text,
+                            fill=self.jgl_color_choice,
+                            font=jgl_font)
+            
+            # Save watermarked photo
+            finished_img = transparent.convert("RGB")
+            jgl_watermarked_image = Image.alpha_composite(self.jgl_original_image, self.jgl_img_copy)
+        except Exception as e:
+            print(f"Can't watermark image, exception: {e}")
+        else:
+            # Shows the output
+            plt.imshow(finished_img)
+            
+        
+            
+            print("Tapped!")
+            print(jgl_watermark_text)   
+        
+            
+    def jgl_open_file(self):
+        """Opens file selector"""  
+        
+        # Opens the file browser to choose an image, suggested by Paul D Bourret on Udemy
+        self.jgl_browsing.set("Searching...")
+        jgl_photo_file = askopenfilename(initialdir=JGL_IMAGE_FOLDER, title="Select A File", filetype=
+        (("jpeg files", "*.jpg"), ("all files", "*.*")))
+        
+        if jgl_photo_file:
+            # If the file is found, modify it
+            
+            try:
+                # Use image.open() for the opening of the image 
+                self.jgl_original_image = Image.open(jgl_photo_file).convert("RGBA")
+                
+                # Opens the photo viewer
+                self.jgl_original_image.show()
+                    
+                # Opens the image in the IDE
+                plt.imshow(self.jgl_original_image)
+                    
+                # Make a copy of an image for the creation of the watermark image with the same dimensions as the older one
+                self.jgl_img_copy = Image.new("RGBA", self.jgl_original_image.size, (255, 255, 255, 0))
+                
+                self.jgl_img_resize = self.jgl_img_copy.resize((round(self.jgl_original_image.size[0]*0.25), 
+                                                    (round(self.jgl_original_image.size[1]*0.25))))
+                self.jgl_img_copy_mask = self.jgl_img_copy.convert("RGBA")
+                    
+            except FileNotFoundError as jgl_not_found:
+                print(f"File not found, exception: {jgl_not_found}")
+            except Exception as e:
+                print(f"Can't open file, exception: {e}")
+            else:
+                # If the image can be opened, proceed to the watermarking process
+                
+                
+                # Set position to lower right corner, suggested by Paul D Bourret on Udemy
+                self.position = (self.jgl_original_image.size[0] - self.jgl_img_resize.size[0], self.jgl_original_image.size[1] - self.jgl_img_resize.size[1])
+            
+                # Make the image editable using ImageDraw
+                self.jgl_drawing = ImageDraw.Draw(self.jgl_img_copy)
+                
+                    
+                
 #-------------- Step 2: Setting up widgets -------------------- 
 # TODO: Use the canvas widget to create a background. 
 # TODO: Set up its size equal to the image dimensions. 
