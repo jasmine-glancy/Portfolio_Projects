fba87cf6990bc9f1372bbc235206cefc2db7a4d5
jglancy95 <jasmine.glancy.vt@gmail.com>
2024-06-24 21:52:11 -0400

    Load in TODOs

---
+++ JGL Flash Card Study Buddy/main.py
@@ -0,0 +1,33 @@
+"""A program that creates flash cards from a database
+using Tkinter and saves the unknown cards to a new list!"""
+
+# Based on day 31 of 100 Days of Code: Complete Python Pro 
+# # Bootcamp by Dr. Angela Yu on Udemy
+
+# TODO: Load in database
+    # Create tables for terminology, Q & A, and expected outputs
+    # User can choose which tables to study from
+
+# ---------------- Flash Card Functionality ---------------- #
+
+# TODO: Create new flash cards
+
+# TODO: Check if the card is known
+
+# TODO: Flip the card
+
+# ------------------------ UI Setup ------------------------ #
+
+# TODO: Create front of card
+
+# TODO: Create back of card
+
+# TODO: Load in wrong button
+
+# TODO: Load in right button
+
+# TODO: Load in heading (Category)
+
+# TODO: Load in question
+
+# TODO: Load in answer
