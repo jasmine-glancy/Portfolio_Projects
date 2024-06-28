6ef51d25fe2f620bc1cd93f234d41acf3918f0f0
jglancy95 <jasmine.glancy.vt@gmail.com>
2024-06-27 23:46:14 -0400

    Add TODOs

---
+++ JGL TODO Website/main.py
@@ -0,0 +1,29 @@
+"""A Todo List website that helps the user keep track of tasks
+they need to get done throughout the week"""
+
+# TODO: Load index page with the calendar
+    # TODO: The calendar should have buttons to add a new task
+
+    # TODO: If the user is logged in and the user has tasks
+        # TODO: Load in bars to represent each TODO of the day
+            # TODO: allow colors to overlap
+            
+    # TODO: Allow the user to "zoom" in on a day and week
+    
+# TODO: Create week view page
+    # TODO: Allow the user to choose what day their week starts from
+        # TODO: Allow the user to edit their previous choice of starting day
+    
+# TODO: Create day view page
+
+# TODO: Create registration page
+
+# TODO: Create login page
+
+# TODO: Create task submission page
+    # TODO: Allow the user to choose task color on the calendar
+    # TODO: Name the task
+    # TODO: Ask for time started and task duration
+    
+
+
