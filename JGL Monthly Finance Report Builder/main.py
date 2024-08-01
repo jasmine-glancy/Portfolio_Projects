28bf0042c9f741773b7a1a2c55c4904ac3bf012a
jglancy95 <jasmine.glancy.vt@gmail.com>
2024-08-01 15:38:31 -0400

    feat: Add TODOs and project docstring

---
+++ JGL Monthly Finance Report Builder/main.py
@@ -0,0 +1,22 @@
+"""A program that scans the excel charge logs for each shift
+to build an emailed report of the top focus areas for training"""
+
+# TODO: Import excel sheets for analysis
+
+# TODO: Count over- and under- charge columns
+
+    # TODO: Count and classify recurring item totals
+    
+    # TODO: Final report should have 5 most missed and/or altered charges
+    
+# TODO: Include focus categories?
+
+    # TODO: Compare billing code database items with reports
+
+# TODO: Include shift totals
+
+# TODO: Include YTD totals per shift
+
+# TODO: Save report as a PDF
+
+# TODO: Email report?
