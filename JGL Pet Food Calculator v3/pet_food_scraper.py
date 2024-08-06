b2f775955386da5aa5b80cce21eae32a20577468
jglancy95 <jasmine.glancy.vt@gmail.com>
2024-08-06 14:25:21 -0400

    Feat: Add pet food scraper TODOs

---
+++ JGL Pet Food Calculator v3/pet_food_scraper.py
@@ -0,0 +1,22 @@
+"""A web scraper that creates a log of currently
+available pet food and treats and logs their caloric
+content"""
+
+# TODO: Scraper should query brands that follow WSAVA guidelines in addition
+## to BEG brands (Boutique, Exotic, Grain-Free)
+
+# TODO: Scrape the main merchant websites for product name, form, and kcal information
+    # TODO: Scrape Chewy.com
+
+    # TODO: Scrape Hillspet.com
+
+    # TODO: Scrape Royalcanin.com
+
+    # TODO: Scrape Purina.com
+
+    # TODO: Scrape Iams.com
+
+    # TODO: Scrape Eukanuba.com
+    
+# TODO: Add information to database if it is not already there
+    
