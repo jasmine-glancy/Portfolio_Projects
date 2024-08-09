f7092a77897d42b49184237f557fafe175bb0e48
jglancy95 <jasmine.glancy.vt@gmail.com>
2024-08-08 20:16:55 -0400

    refact: Add TODOs for each website scraper

--- JGL Pet Food Calculator v3/pet_food_scraper.py
+++ JGL Pet Food Calculator v3/pet_food_scraper.py
@@ -2,21 +2,154 @@
 available pet food and treats and logs their caloric
 content"""
 
+import os
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+
+load_dotenv("D:/Python/EnvironmentVariables/.env")
+
+RC_DOG_FOOD_SEARCH = "https://www.royalcanin.com/us/dogs/products/retail-products"
+HILLS_DOG_FOOD_SEARCH = "https://www.hillspet.com/products/dog-food"
+IAM_DRY_DOG_FOOD_SEARCH = "https://www.iams.com/dog/dog-food/dry"
+EUKANUBA_FOOD_SEARCH = "https://www.eukanuba.com/us/all-products"
+
+class JgWebScraper:
+    def __init__(self):
+        # Create the web driver
+        
+        options = Options()
+                
+        # Option to keep Chrome open
+        options.add_experimental_option("detach", True)  
+        
+        # Use a temporary profile to avoid conflicts
+        options.add_argument("--incognito")
+        
+        # Ignore SSL certificate errors
+        options.add_argument("--ignore-certificate-errors")
+        
+        # Set a custom User-Agent string
+        options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
+
+        
+        self.driver = webdriver.Chrome(options=options)
+        
+    def rc_dog_food_search(self):
+        # Navigate to the specified URL
+        print(f"Navigating to {RC_DOG_FOOD_SEARCH}")
+        self.driver.get(RC_DOG_FOOD_SEARCH)
+        
+        time.sleep(5)
+        # Verify the correct page is loaded
+        current_url = self.driver.current_url
+        print(f"Current URL: {current_url}")
+        
+        # TODO: For each result in the page, "click" on result 
+        
+            # TODO: "click" on "nutritional information" 
+            
+                # TODO: "click" on "calorie content"
+        
+                # TODO: add caloric content to database
+                
+            # TODO: "click" on "ingredients" 
+                # TODO: pick protein sources out of the ingredient list 
+                    # TODO: add protein sources to database
+                    # TODO: if protein sources contain a common allergen, add to db
+            
+        # TODO: if at the end of the result page, click on the next page
+            # TODO: If no "next" page, stop scraping
+        
+    def hills_dog_food_search(self):
+        # Navigate to the specified URL
+        print(f"Navigating to {HILLS_DOG_FOOD_SEARCH}")
+        self.driver.get(HILLS_DOG_FOOD_SEARCH)
+        
+        time.sleep(5)
+        # Verify the correct page is loaded
+        current_url = self.driver.current_url
+        print(f"Current URL: {current_url}")
+        
+        # Click truste-consent-button to consent to cookies
+                
+        # TODO: For each result in the page, "click" on result 
+        
+            # TODO: "click" on "average nutrient and caloric content" 
+        
+                # TODO: add caloric content to database
+                
+            # TODO: "click" on "ingredients" 
+                # TODO: pick protein sources out of the ingredient list 
+                    # TODO: add protein sources to database
+                    # TODO: if protein sources contain a common allergen, add to db
+            
+        # TODO: if at the end of the result page, click on the next page
+            # TODO: If no "next" page, stop scraping
+            
+    def iams_dry_dog_food_search(self):
+        
+        # Navigate to the specified URL
+        print(f"Navigating to {IAM_DRY_DOG_FOOD_SEARCH}")
+        self.driver.get(IAM_DRY_DOG_FOOD_SEARCH)
+        
+        time.sleep(5)
+        # Verify the correct page is loaded
+        current_url = self.driver.current_url
+        print(f"Current URL: {current_url}")
+        
+        # TODO: For each result in the page, "click" on result 
+        
+            # TODO: "click" on "ingredients" 
+        
+                # TODO: add caloric content to database
+                
+                # TODO: pick protein sources out of the ingredient list 
+                    # TODO: add protein sources to database
+                    # TODO: if protein sources contain a common allergen, add to db
+            
+        # TODO: if at the end of the result page, click on the next page
+            # TODO: If no "next" page, stop scraping
+            
+    def eukanuba_food_search(self):
+        
+        # Navigate to the specified URL
+        print(f"Navigating to {EUKANUBA_FOOD_SEARCH}")
+        self.driver.get(EUKANUBA_FOOD_SEARCH)
+        
+        time.sleep(5)
+        # Verify the correct page is loaded
+        current_url = self.driver.current_url
+        print(f"Current URL: {current_url}")
+        
+        # TODO: For each result in the page, "click" on result 
+        
+            # TODO: "click" on "ingredients" 
+        
+                # TODO: add caloric content to database
+                
+                # TODO: pick protein sources out of the ingredient list 
+                    # TODO: add protein sources to database
+                    # TODO: if protein sources contain a common allergen, add to db
+            
+        # TODO: if at the end of the result page, click on the next page
+            # TODO: If no "next" page, stop scraping
+                
+                
+jg_web_scraper = JgWebScraper()
+# # jg_web_scraper.rc_dog_food_search()
+# jg_web_scraper.hills_dog_food_search()
+# jg_web_scraper.iams_dry_dog_food_search()
+jg_web_scraper.eukanuba_food_search()
 # TODO: Scraper should query brands that follow WSAVA guidelines in addition
 ## to BEG brands (Boutique, Exotic, Grain-Free)
 
 # TODO: Scrape the main merchant websites for product name, form, and kcal information
-    # TODO: Scrape Chewy.com
-
-    # TODO: Scrape Hillspet.com
-
-    # TODO: Scrape Royalcanin.com
-
-    # TODO: Scrape Purina.com
-
-    # TODO: Scrape Iams.com
-
-    # TODO: Scrape Eukanuba.com
     
 # TODO: Add information to database if it is not already there
     
