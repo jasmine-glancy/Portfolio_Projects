ff8f2ee63316adf68dcfd03e17edbe130156177c
jglancy95 <jasmine.glancy.vt@gmail.com>
2024-08-23 23:32:48 -0400

    feat: Add TODOs

---
+++ JGL Pet Food Calculator v3/USDA_food_database_api.py
@@ -0,0 +1,21 @@
+"""A program that calls the USDA FoodData Central API to provide amounts
+of human food to give to dogs and cats without unbalancing their diet"""
+
+# TODO: Prompt user to give a human food to look up in the USDA food database
+
+# TODO: Call the string against USDA API via /foods/search
+
+# TODO: Have the user select an option
+
+# TODO: Call max treat amount per day from pet_food_calculator.db
+
+    # TODO: Display human food treat amounts on the final report
+    # # Database table? ID corresponding to a FK to USDA Treat Amounts table
+    # # Limit to one option for now
+    
+    # TODO: Provide disclaimers
+    
+        # TODO: If the pet can have a large amount (i.e. >1 cup) of a certain type of
+        # # human food, advise user to check with their vet that this amount will 
+        # # cause no expected health issues 
+    
