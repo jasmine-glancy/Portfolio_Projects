0da1dc3753cf5a7e9d5670c3954d12a14e698b3e
jglancy95 <jasmine.glancy.vt@gmail.com>
2024-08-31 02:13:01 -0400

    feat: Call FoodCentral Database for information on a human food, print results in terminal. UI integration WIP

--- JGL Pet Food Calculator v3/USDA_food_database_api.py
+++ JGL Pet Food Calculator v3/USDA_food_database_api.py
@@ -5,12 +5,16 @@ source:
 U.S. Department of Agriculture, Agricultural Research Service. FoodData Central, 2019. fdc.nal.usda.gov"""
 
 # Using Blueprint 
-from flask import Blueprint, render_template, request
+from flask import Blueprint, flash, render_template, url_for, \
+    redirect, request
+import os 
+import requests
 
 # Define the Blueprint
 human_foods = Blueprint("human_foods", __name__)
 
-FOOD_DATA_CENTRAL_URL = "https://api.nal.usda.gov/fdc/v1/foods/search"
+USDA_API_KEY = os.environ.get("USDA_API_KEY")
+FOOD_DATA_CENTRAL_URL = f"https://api.nal.usda.gov/fdc/v1/foods/search"
 
 @human_foods.route("/search_human_foods", methods=["GET", "POST"])
 def search_human_foods():
@@ -18,12 +22,57 @@ def search_human_foods():
     the USDA's food database"""
 
     if request.method == "POST":
-
-        # TODO: Call the string against USDA API via /foods/search
-
-            # TODO: If the string matches anything in the USDA API, call a list
+        print(request.form.get("food_name"))
         
+        params = {
+            "api_key": USDA_API_KEY,
+            "query":request.form.get("food_name")
+        }
+        
+        # TODO: Call the string against USDA API via /foods/search
+        fdc_response = requests.get(FOOD_DATA_CENTRAL_URL, params=params)
+        
+        if fdc_response.status_code == 200:
+            # TODO: If the string matches anything in the USDA API, call a list
+            data = fdc_response.json()
+            found_foods = []
+            
+            for item in data.get("foods", []):
+                description = item.get("description", "No description found")
+                brand_name = item.get("brandName", "No brand name")
+                
+                energy_value = None
+                energy_unit = None
+                
+                for nutrient in item.get("foodNutrients", []):
+                    print(f"nutrient name: {nutrient.get("nutrientName")}, Nutrient value: {nutrient.get("value")}")
+                    if nutrient.get("nutrientName") == "Energy":
+                        energy_value = nutrient.get("value")
+                        energy_unit = nutrient.get("unitName", [])
+                        break
+                    
+                print(f"Description: {description}")
+                if energy_value is not None and energy_unit is not None:
+                    print(f"Energy: {energy_value} {energy_unit}")
+                    
+                    food_info = {
+                        "description": description,
+                        "brandName": brand_name,
+                        "energy": f"{energy_value} {energy_unit}" if energy_value and energy_unit else "Energy information not available"
+                    }
+                    
+                    found_foods.append(food_info)
+                    
+                else:
+                    print("Energy information not available")
+                
+                print(f"Found foods: {found_foods}")
+                return redirect(url_for("human_foods.search_human_foods", foods=found_foods))
+        else:
             # TODO: Otherwise, provide an error message
+            flash("Food not found. Please try again!")
+            
+            return redirect(url_for("human_foods.search_human_foods"))
         pass
     
     
