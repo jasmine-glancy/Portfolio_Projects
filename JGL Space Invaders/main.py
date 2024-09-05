d6d6b1bdf9c6d6fd0d94e0b744c4d14a969eca57
jglancy95 <jasmine.glancy.vt@gmail.com>
2024-09-05 19:51:10 -0400

    feat: Add TODOs

---
+++ JGL Space Invaders/main.py
@@ -0,0 +1,38 @@
+"""A Python version of the 1978 shoot 'em up game Space Invaders!"""
+
+# TODO: Create laser cannon
+
+# TODO: Enable laser cannon to move left and right
+
+# TODO: Enable laser to shoot out of the cannon when space bar is pressed
+
+# TODO: Create "aliens" 
+
+    # TODO: Five rows of eleven
+    
+    # TODO: Aliens move left and right as a group
+    
+    # TODO: Aliens move downward toward the shooter each time they touch the 
+    # # edge of the screen
+    
+    # TODO: Aliens fire projectiles toward the player
+    
+    # TODO: A special "mystery ship" occasionally moves across the top of the screen
+        # TODO: This ship rewards bonus points if hit
+    
+# TODO: Create bunkers
+
+    # TODO: Bunkers are gradually destroyed from the top by the alien lasers
+    
+    # TODO: If the player fires underneath the bunker, the bottoms get destroyed
+    
+# TODO: Player has 3 lives
+    # TODO: But the game ends immediately if the aliens reach the bottom of the screen
+    
+    
+# TODO: The goal is to eliminate all of the aliens by shooting them
+
+    # TODO: As the aliens are defeated, their movement and the music speed up
+
+    # TODO: When all the aliens are defeated, bring another wave which starts lower
+        # Loop continues
