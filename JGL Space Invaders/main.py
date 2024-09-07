b5d0a36e3bf492b7c10955c33991c59fd57881df
jglancy95 <jasmine.glancy.vt@gmail.com>
2024-09-07 00:24:25 -0400

    feat: Create screen

--- JGL Space Invaders/main.py
+++ JGL Space Invaders/main.py
@@ -1,6 +1,12 @@
 """A Python version of the 1978 shoot 'em up game Space Invaders!"""
 
+from turtle import Screen
+
 # TODO: Create screen
+jgl_screen = Screen()
+jgl_screen.setup(width=950, height=750)
+jgl_screen.bgcolor("black")
+jgl_screen.title("Space Invaders by JGL")
 
 # TODO: Create cannon
 
@@ -18,3 +24,5 @@
 
     # TODO: When all the aliens are defeated, bring another wave which starts lower
         # Loop continues
+        
+jgl_screen.exitonclick()
