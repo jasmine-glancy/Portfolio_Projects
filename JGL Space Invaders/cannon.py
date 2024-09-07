3ac9158a6e2ab41b0b8d1406322b168eeb012ee2
jglancy95 <jasmine.glancy.vt@gmail.com>
2024-09-07 00:48:31 -0400

    feat: Creates the cannon for Space Invaders and enables it to move left or right

--- JGL Space Invaders/cannon.py
+++ JGL Space Invaders/cannon.py
@@ -2,10 +2,47 @@
 
 from turtle import Turtle
 
+MOVE_STEPS = 20
+
 class JglCannon(Turtle):
     """Creates the cannon and allows it to move"""
-# TODO: Create laser cannon
+    
+    def __init__(self) -> None:
+        """Creates the laser cannon"""
+        
+        super().__init__()
+        self.penup()
+        self.goto((0, -300))
+        self.shape("square")
+        self.color("lime")
+        self.shapesize(stretch_len=4, stretch_wid=2)
+        self.penup()
+        
+        self.cannon_top = Turtle()
+        self.cannon_top.penup()
+        self.cannon_top.goto((0, -275))
+        self.cannon_top.shape("square")
+        self.cannon_top.color("lime")
+        self.cannon_top.shapesize(stretch_len=1.5, stretch_wid=2)
 
-# TODO: Enable laser cannon to move left and right
+    def jgl_move_paddle_left(self) -> None:
+        jgl_new_x_pos = self.xcor() - MOVE_STEPS
+        
+        if jgl_new_x_pos < -425:
+            """Bounces the paddle back if too far to the left"""
+            jgl_new_x_pos = -425
+        self.goto(jgl_new_x_pos, self.ycor())        
+        self.cannon_top.goto(jgl_new_x_pos, self.cannon_top.ycor()) 
+
+        
+    def jgl_move_paddle_right(self) -> None:
+        jgl_new_x_pos = self.xcor() + MOVE_STEPS
+        
+        if jgl_new_x_pos > 425:
+            """Bounces the paddle back if too far to the right"""
+            jgl_new_x_pos = 425
+        self.goto(jgl_new_x_pos, self.ycor())
+        self.cannon_top.goto(jgl_new_x_pos, self.cannon_top.ycor())
+        
 
 # TODO: Enable laser to shoot out of the cannon when space bar is pressed
--- JGL Space Invaders/main.py
+++ JGL Space Invaders/main.py
@@ -1,18 +1,34 @@
 """A Python version of the 1978 shoot 'em up game Space Invaders!"""
 
+from cannon import JglCannon
 from turtle import Screen
 
-# TODO: Create screen
+# Create screen
 jgl_screen = Screen()
 jgl_screen.setup(width=950, height=750)
 jgl_screen.bgcolor("black")
 jgl_screen.title("Space Invaders by JGL")
 
-# TODO: Create cannon
+
+# Turn off automatic screen updates
+jgl_screen.tracer(0)
+
+# Create cannon
+jgl_cannon = JglCannon()
 
 # TODO: Create "aliens" 
 
 # TODO: Create bunkers
+
+# Event listeners to check for user input
+jgl_screen.listen()
+
+# Movement is controlled by the "A" and "D" keys
+jgl_screen.onkeypress(jgl_cannon.jgl_move_paddle_left, "a")
+jgl_screen.onkeypress(jgl_cannon.jgl_move_paddle_right, "d")
+
+# Set game flag
+jgl_game_on = True
 
 # TODO: Player has 3 lives
     # TODO: But the game ends immediately if the aliens reach the bottom of the screen
@@ -24,5 +40,8 @@ jgl_screen.title("Space Invaders by JGL")
 
     # TODO: When all the aliens are defeated, bring another wave which starts lower
         # Loop continues
-        
+
+while jgl_game_on:
+    jgl_screen.update()
+    
 jgl_screen.exitonclick()
