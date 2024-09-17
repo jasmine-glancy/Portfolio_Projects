1d95baaa9fc351225e2816e71b1bce845224f00b
jglancy95 <jasmine.glancy.vt@gmail.com>
2024-09-17 18:11:27 -0400

    feat: Add favicon, update about me info and pictures

--- JGL Online Shop/main.py
+++ JGL Online Shop/main.py
@@ -26,22 +26,52 @@ def current_year():
     
     return jgl_current_year
 
+def social_links():
+    """Returns the social media links"""
+    
+    github = os.environ.get("github")
+    linkedin = os.environ.get("linkedin")
+    discord = os.environ.get("discord")
+    fiverr = os.environ.get("fiverr")
+    email = os.environ.get("email")
+    youtube = os.environ.get("youtube")
+    
+    socials = {
+        "github_link": github,
+        "linkedin_link": linkedin,
+        "discord_link": discord,
+        "fiverr_link": fiverr,
+        "email_address": email,
+        "youtube_link": youtube
+        }
+    
+    return socials
+
 JGL_CURRENT_YEAR = current_year()
+JGL_SOCIALS = social_links()
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     """Shows homepage with a current copyright date"""
     
-    return render_template("index.html", date=JGL_CURRENT_YEAR)
+
+    
+    return render_template("index.html",
+                           date=JGL_CURRENT_YEAR,
+                           socials=JGL_SOCIALS)
 
 @app.route("/products", methods=["GET", "POST"])
 def products():
     """Showcases a list of products to buy"""
     
-    return render_template("products.html", date=JGL_CURRENT_YEAR)
+    return render_template("products.html", 
+                           date=JGL_CURRENT_YEAR,
+                           socials=JGL_SOCIALS)
 
 @app.route("/products/<int:product_id>", methods=["GET", "POST"])
 def for_sale_info():
     """Provides more info on each product"""
     
-    return render_template("product_page.html", date=JGL_CURRENT_YEAR)
+    return render_template("product_page.html",
+                           date=JGL_CURRENT_YEAR,
+                           socials=JGL_SOCIALS)
