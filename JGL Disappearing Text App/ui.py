b16d3762a1e3930c1f3144532420155a8c3427b0
jglancy95 <jasmine.glancy.vt@gmail.com>
2025-02-08 23:10:31 -0500

    Add TODOs

--- JGL Disappearing Text App/ui.py
+++ JGL Disappearing Text App/ui.py
@@ -4,7 +4,7 @@
 import tkinter as tk
 
 class JglWritersBlockBuster():
-    def __init__(self) -> None:
+    def __init__(self):
         """Loads the window and the functions"""
         
         self.jgl_window = tk.Tk()
@@ -14,7 +14,7 @@ class JglWritersBlockBuster():
         self.jgl_window.config(padx=20, pady=20, background=self.russian_violet)
         self.jgl_window.mainloop()
 
-    def jgl_load_colors(self) -> str:
+    def jgl_load_colors(self):
         """Loads in colors from https://coolors.co/palette/f4effa-2f184b-532b88-9b72cf-c8b1e4"""
         
         self.magnolia = "#F4EFFA"
@@ -22,5 +22,13 @@ class JglWritersBlockBuster():
         self.tekhelet = "#532B88"
         self.amethyst = "#9B72CF"
         self.wisteria = "#C8B1E4"
-        
+    
 
+# TODO: When the start button is clicked, the application begins
+
+# TODO: Load a cursor that moves to reveal typed text
+
+# TODO: As long as the user continues to type with breaks no longer than 
+# # 5-10 seconds, the application continues
+
+# TODO: If the exercise ends, display "The time ran out, killing your darlings!"
