f05fa4f6f0f630d830418ee52371a76a3b6ed914
jglancy95 <jasmine.glancy.vt@gmail.com>
2025-02-12 01:13:14 -0500

    Complete version 1

--- JGL Disappearing Text App/ui.py
+++ JGL Disappearing Text App/ui.py
@@ -16,11 +16,15 @@ class JglWritersBlockBuster():
         self.jgl_window.title("Writer's Block Buster")
         self.jgl_window.geometry("700x500")
         self.jgl_load_colors()
-        self.jgl_instructions()
-        self.jgl_start_button()
+        self.jgl_start_page()
         self.jgl_window.config(padx=20, pady=20, background=self.russian_violet)
         self.jgl_window.mainloop()
 
+    def jgl_start_page(self):
+        """Load starting instructions and button"""
+        self.jgl_instructions()
+        self.jgl_start_button()    
+        
     def jgl_load_colors(self):
         """Loads in colors from https://coolors.co/palette/f4effa-2f184b-532b88-9b72cf-c8b1e4"""
         
@@ -76,7 +80,7 @@ class JglWritersBlockBuster():
         """Starts the application"""
         
         # Set game flag
-        self.game_on = True
+        self.app_on = True
         
         self.text = ""
         self.timer = None
@@ -96,14 +100,14 @@ class JglWritersBlockBuster():
         self.typing_area = tk.Text(
             font=("Helvetica Monospaced", 12, "normal"),
             bg=self.tekhelet, fg=self.amethyst,
-            width=72, height=18, wrap="w",
+            width=72, height=14, wrap="w",
             highlightthickness=0,
             padx=5, pady=5 
             )
         
-        self.typing_area.bind('<KeyPress>', self.start_timer)
-        self.typing_area.place(relx=0, rely=0.28)
-        
+        self.typing_area.bind("<KeyPress>", self.start_timer)
+        self.typing_area.place(relx=0, rely=0.2)
+                
     def start_timer(self, event):
         """
          As long as the user continues to type with breaks no 
@@ -116,11 +120,19 @@ class JglWritersBlockBuster():
         # Use keysym to keep track of which key was pressed
         if event.keysym == "BackSpace":
             self.text = self.text[0: len(self.text) - 1]
+            self.jgl_timer_countdown()
             
         elif event.char:
             # Adds typed text for display
             self.text += event.char
-            self.timer = self.jgl_window.after(5000, self.reset_app)
+            self.jgl_timer_countdown()
+        
+    def jgl_timer_countdown(self):
+        """
+        Changes text color based on how long it's been since the user has typed
+        """    
+        self.jgl_window.after(3000, self.jgl_fade_text)
+        self.timer = self.jgl_window.after(5000, self.reset_app)
         
     def reset_app(self):
         """Deletes the typed text and notifies the user"""
@@ -129,12 +141,38 @@ class JglWritersBlockBuster():
         self.text = ""
         self.timer = None   
         
-        jgl_notification = tk.Label()
-        jgl_notification.config(
+        self.jgl_notification = tk.Label()
+        self.jgl_notification.config(
             text="The time ran out, killing your darlings!",
             font=FONT_CHOICE,
             bg=self.russian_violet, fg=self.amethyst,
         )
-        jgl_notification.place(relx=0.5, rely=0.15, anchor=CENTER)
+        self.jgl_notification.place(relx=0.5, rely=0.1, anchor=CENTER)
 
-# TODO: Make text slowly fade away
+        self.jgl_end_button()
+        
+    def jgl_fade_text(self):
+        self.typing_area.configure(fg="#774eac")
+        
+    def jgl_end_button(self):
+        
+        self.end_button = tk.Button(text="Try Again",
+                                      width=35,
+                                      highlightthickness=0,
+                                      bg=self.tekhelet,
+                                      fg=self.wisteria,
+                                      activebackground=self.amethyst,
+                                      activeforeground=self.magnolia,
+                                      pady=10,
+                                      padx=10,
+                                      font=FONT_CHOICE,
+                                      command=self.jgl_restart_app)
+        self.end_button.place(relx=0.5, rely=0.9, anchor=CENTER)
+    
+    def jgl_restart_app(self):
+        self.typing_area.destroy()
+        self.end_button.destroy()
+        self.jgl_notification.destroy()
+        
+        self.jgl_start_page()
+        
