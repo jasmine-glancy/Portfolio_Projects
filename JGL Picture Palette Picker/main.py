01de97bbe9aa5cf1cc26ecb46d4d1861aff8f9af
jglancy95 <jasmine.glancy.vt@gmail.com>
2025-02-26 18:05:01 -0500

    initial commit, add TODOs

---
+++ JGL Picture Palette Picker/main.py
@@ -0,0 +1,10 @@
+"""
+A website that picks the top 10 most common colors in an uploaded image
+"""
+
+import flask as f
+import numpy
+
+# TODO: Allow user to upload an image
+
+# TODO: Use NumPy to find the most common colors in the uploaded image
