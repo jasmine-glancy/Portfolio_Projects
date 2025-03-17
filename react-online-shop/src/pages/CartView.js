627225c18d238b941818ef41d035e3e0894c0c7a
jglancy95 <jasmine.glancy.vt@gmail.com>
2025-03-17 00:11:00 -0400

    refactor(UI): adjust cart view column a, begin styling column b

--- react-online-shop/src/pages/CartView.js
+++ react-online-shop/src/pages/CartView.js
@@ -6,9 +6,9 @@ import Input from "../components/Input";
 function CartView () {
     
     let inputStyle = {
-        width: "100%",
+        width: "700px",
         padding: "3% 10%",
-        margin: "3% 0 10%"
+        margin: "3% 0%"
     };
 
     let cityInputStyle = {
@@ -24,25 +24,27 @@ function CartView () {
     
 
     let btnStyle1 = {
-        padding: "2% 7%",
-        width: "260%",
+        padding: "3% 7%",
+        width: "460%",
         color: "#000",
         background: "#fff",
         border: "0.5px solid #000",
-        fontWeight: 100,
+        fontWeight: "100",
         fontSize: "1.5rem",
-        margin: "5% 7%"
+        margin: "5% 3%",
+        cursor: "pointer",
     };
 
     let btnStyle2 = {
         padding: "2% 7%",
         border: "none",
-        width: "118%",
+        width: "142%",
         color: "#fff",
         background: "rgb(156, 156, 156)",
-        fontWeight: 100,
+        fontWeight: "100",
         fontSize: "1.5rem",
-        margin: "2% 3%"
+        margin: "2% 3%",
+        cursor: "pointer",
     };
 
     return (
@@ -50,27 +52,53 @@ function CartView () {
             <NavBar />
             <div id="cart-cont"> 
                 <div id="cart-view">
-                   <form id="cart-form">
-                        <Input labelName={"City"} placeholder={"Enter City"} labelStyle={labelStyle} inputStyle={cityInputStyle} />
-                        <Input labelName={"Address"} placeholder={"Enter Address"}  labelStyle={labelStyle} inputStyle={inputStyle} />
-                        <Input labelName={"Recipient Details"} placeholder={"Enter Recipient Details"}  labelStyle={labelStyle} inputStyle={inputStyle} />
-                        <Input labelName={"Phone"} placeholder={"Enter Phone"}  labelStyle={labelStyle} inputStyle={inputStyle} />
-                        <Input labelName={"Email"} placeholder={"Enter Email"}  labelStyle={labelStyle} inputStyle={inputStyle} />
+                   <form id="cart-form-cont">
+                        <div id="cart-form-a">
+                            <Input labelName={"City"} placeholder={"Enter City"} labelStyle={labelStyle} inputStyle={cityInputStyle} />
+                            <Input labelName={"Address"} placeholder={"Enter Address"}  labelStyle={labelStyle} inputStyle={inputStyle} />
+                            <Input labelName={"Recipient Details"} placeholder={"Enter Recipient Details"}  labelStyle={labelStyle} inputStyle={inputStyle} />
+                            <Input labelName={"Phone"} placeholder={"Enter Phone"}  labelStyle={labelStyle} inputStyle={inputStyle} />
+                            <Input labelName={"Email"} placeholder={"Enter Email"}  labelStyle={labelStyle} inputStyle={inputStyle} />
 
-                        <div>
-                            <h2> Payment Method </h2>
-                            
-                            <div id="payment-buttons">
-                                <Button buttonStyle={btnStyle1} buttonName={"Payment Card"} />
-                                <Button buttonStyle={btnStyle1} buttonName={"Cash on Delivery"} />
+                            <div>
+                                <h2> Payment Method </h2>
+                                
+                                <div id="payment-buttons">
+                                    <Button buttonStyle={btnStyle1} buttonName={"Payment Card"} />
+                                    <Button buttonStyle={btnStyle1} buttonName={"Cash on Delivery"} />
+                                </div>
                             </div>
-                        </div>
 
-                        <div id="agree">
-                            <input type="checkbox" />
-                            <p>I agree to the terms of the offer and the loyalty policy</p>
+                            <div id="agree">
+                                <input type="checkbox" />
+                                <p>I agree to the terms of the offer and the loyalty policy</p>
+                            </div>
+                            <Button buttonStyle={btnStyle2} buttonName={"Place an Order"} />
                         </div>
-                        <Button buttonStyle={btnStyle2} buttonName={"Place an Order"} />
+                        <div id="cart-form-b">
+                            <div>product box</div>
+
+                            <table id="line-item-prices">
+                                <td>
+                                    <tr>Summary:</tr>
+                                    <tr>Delivery:</tr>
+                                    <tr>Discounts:</tr>
+                                </td>
+                                <td>
+                                    <tr>€340</tr>
+                                    <tr>€0</tr>
+                                    <tr>€0</tr>
+                                </td>
+                                <tfoot>
+                                    <td>
+                                        <tr>Total: </tr>
+                                    </td>
+                                    <td>
+                                        <tr>€340</tr>
+                                    </td>
+                                </tfoot>
+                            </table>
+                        </div>
                    </form>
                 </div>
 
