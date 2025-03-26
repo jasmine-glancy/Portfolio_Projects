f3fa735776faf66bbeac453bc7454024382d7c44
jglancy95 <jasmine.glancy.vt@gmail.com>
2025-03-25 21:43:36 -0400

    initial commit

---
+++ JGL Automated Google Dinosaur Game/main.py
@@ -0,0 +1,14 @@
+""" 
+Automates the Google Dinosaur Game!
+"""
+
+# TODO: Open https://elgoog.im/dinosaur-game/
+
+# TODO: When a pterodactyl or cactus is within range, the t-rex jumps
+
+# TODO: When the t-rex hits the cactus, the game ends
+
+# TODO: Keep track of score
+
+    # TODO: Each milisecond should add 1 to the score
+    
