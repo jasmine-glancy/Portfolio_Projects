b25334e48c7aebbc0c31662821be42dedf45de34
jglancy95 <jasmine.glancy.vt@gmail.com>
2025-03-25 21:57:53 -0400

    feat(main): Open the Google Dino webiste with Selenium

--- JGL Automated Google Dinosaur Game/main.py
+++ JGL Automated Google Dinosaur Game/main.py
@@ -2,7 +2,27 @@
 Automates the Google Dinosaur Game!
 """
 
-# TODO: Open https://elgoog.im/dinosaur-game/
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+DINO_GAME_URL = "https://elgoog.im/dinosaur-game/"
+
+# Set up Selenium
+
+options = Options()
+
+options.add_experimental_option("detach", True)
+options.add_argument("--incognito")
+
+driver = webdriver.Chrome(options=options)
+
+# Open https://elgoog.im/dinosaur-game/
+
+driver.get(DINO_GAME_URL)
+
+# TODO: Use PyAutoGUI to control the mouse/keyboard
+
+# TODO: Use Pillow to process the pictures to jump over
 
 # TODO: When a pterodactyl or cactus is within range, the t-rex jumps
 
